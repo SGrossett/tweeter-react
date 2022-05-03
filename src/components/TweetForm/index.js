182e545d0bf124fa0abd58e2002a93ef40c56644
SGrossett <selene_816@hotmail.com>
2022-05-03 11:43:24 -0400

    Remove unuse js files. Create a component for the TweetList section. Add the given markup and style rules.

--- src/components/TweetForm/index.js
+++ src/components/TweetForm/index.js
@@ -5,16 +5,16 @@ import React from 'react'
 function TweetForm() {
   return (
     <section class="new-tweet">
-        <h2>Compose Tweet</h2>
-        <form action="/tweets/" method="POST">
-          <label for="tweet-text">What are you humming about?</label>
-          <textarea name="text" id="tweet-text"></textarea>
-          <div>
-            <button type="submit">Tweet</button>
-            <span name="counter" class="counter" for="tweet-text">140</span>
-          </div>
-        </form>
-      </section>
+      <h2>Compose Tweet</h2>
+      <form action="/tweets/" method="POST">
+        <label for="tweet-text">What are you humming about?</label>
+        <textarea name="text" id="tweet-text"></textarea>
+        <div>
+          <button type="submit">Tweet</button>
+          <span name="counter" class="counter" for="tweet-text">140</span>
+        </div>
+      </form>
+    </section>
   )
 }
 
