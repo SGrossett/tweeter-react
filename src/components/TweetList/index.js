1fa7d99a5a616c891291054f7a3e0a3f930c0a2d
SGrossett <selene_816@hotmail.com>
2022-05-03 14:46:05 -0400

    Map ove the tweet module in order to render the tweets on the app.

--- src/components/TweetList/index.js
+++ src/components/TweetList/index.js
@@ -1,57 +1,38 @@
 import './tweetList.css';
 
+const tweets = require('../Tweets/Tweet.js');
+console.log('tweets:', tweets.tweets);
+
 function TweetList(){
   return (
     <section className="tweet-container">
-      <article className="tweet">
-        <header>
-          <div className="profile">
-            <img className="profile-pic" src="https://i.imgur.com/73hZDYK.png" />
-            <h4>Newton</h4>
-          </div>
-          
-          <div className="handle">
-            <h5>@SirIsaac</h5>
-          </div>
-        </header>
-        <div>
-          <p>If I have seen further it is by standing on the shoulders of giants</p>
-          <hr />
-        </div>
-        <footer>
-          <div><b>10 days ago</b></div>
+      
+      {tweets.tweets.map((tweet) => (
+        <article className="tweet">
+          <header>
+            <div className="profile">
+              <img className="profile-pic" src={tweet.avatar} />
+              <h4>{tweet.name}</h4>
+            </div>
+            
+            <div className="handle">
+              <h5>{tweet.handle}</h5>
+            </div>
+          </header>
           <div>
-            <button className="icon flag"><i className="fas fa-flag"></i></button>
-            <button className="icon retweet"><i className="fas fa-retweet"></i></button>
-            <button className="icon heart"><i className="fas fa-heart"></i></button>
+            <p>{tweet.body}</p>
+            <hr />
           </div>
-        </footer>
-      </article>
-
-      <article className="tweet">
-        <header>
-          <div className="profile">
-            <img className="profile-pic" src="https://i.imgur.com/nlhLi3I.png" />
-            <h4>Descartes</h4>
-          </div>
-          
-          <div className="handle">
-            <h5>@rd</h5>
-          </div>
-        </header>
-        <div>
-          <p>Je pense , donc je suis</p>
-          <hr />
-        </div>
-        <footer>
-          <div><b>10 days ago</b></div>
-          <div>
-            <button className="icon flag"><i className="fas fa-flag"></i></button>
-            <button className="icon retweet"><i className="fas fa-retweet"></i></button>
-            <button className="icon heart"><i className="fas fa-heart"></i></button>
-          </div>
-        </footer>
-      </article>
+          <footer>
+            <div><b>{tweet.age}</b></div>
+            <div>
+              <button className="icon flag"><i className="fas fa-flag"></i></button>
+              <button className="icon retweet"><i className="fas fa-retweet"></i></button>
+              <button className="icon heart"><i className="fas fa-heart"></i></button>
+            </div>
+          </footer>
+        </article>
+      ))}
     </section>
   );
 }
