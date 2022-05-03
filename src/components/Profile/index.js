c8455b6e2c2eae156f6b5fe8dc9cc2f4bfcab982
SGrossett <selene_816@hotmail.com>
2022-05-03 11:19:11 -0400

    Add the markup and styling for the header (Profile) component.

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,10 +1,25 @@
-import './profile.css';
-
-import React from 'react'
+import './css/profile.css';
+import profilePic from './images/luffy-wave.png';
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <header>
+      <div class='profile-info'>
+        <div class='header-img'>
+          <img src={profilePic} /> 
+        </div>
+        <div class='name'>
+            <h2>Pirate King Luffy</h2>
+        </div>
+      </div>
+      
+
+        <div class='dark-mode'>
+          <button class='icon btn-toggle'>
+            <i class='fas fa-moon'></i>
+          </button>
+        </div>
+    </header>
   )
 }
 
